5c3069158a199eb76b90d49cfc6deeb1d8ee2492
Markusstr <markus.strandman@gmail.com>
2019-12-12 22:08:51 +0200

    More actions in loggedReducer

--- frontend/src/actions/logged.js
+++ frontend/src/actions/logged.js
@@ -1,6 +1,30 @@
 
-export function changeLoggedState() {
+export function setLoggedState() {
     return {
         type: "SIGN_IN"
+    };
+}
+
+export function removeLoggedState() {
+    return {
+        type: "SIGN_OUT"
+    };
+}
+
+export function setUsername(username) {
+    return {
+        type: "SET_USERNAME",
+        payload: {
+            username
+        }
+    };
+}
+
+export function setSessionID(sessionID) {
+    return {
+        type: "SET_SESSION_ID",
+        payload: {
+            sessionID
+        }
     };
 }
--- frontend/src/reducers/logged.js
+++ frontend/src/reducers/logged.js
@@ -1,12 +1,22 @@
 const initialState = {
     loggedState: false,
-    username: ""
+    username: "",
+    sessionID: ""
 };
 
 export function loggedReducer(state = initialState, action) {
     switch (action.type) {
         case "SIGN_IN":
             state.loggedState = true;
+            return state;
+        case "SIGN_OUT":
+            state.loggedState = false;
+            return state;
+        case "SET_USERNAME":
+            state.username = action.payload.username;
+            return state;
+        case "SET_SESSION_ID":
+            state.sessionID = action.payload.sessionID;
             return state;
         default:
             return state;

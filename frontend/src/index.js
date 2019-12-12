a21fdf58615f7d3938bb7a271cdb50d0d280f348
Markusstr <markus.strandman@gmail.com>
2019-12-12 22:08:10 +0200

    Removed postReducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
-import { postsReducer } from "./reducers/posts"
 import { loggedReducer } from "./reducers/logged"
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 const store = createStore(
     combineReducers({
-        postsReducer,
         loggedReducer
     }),
     applyMiddleware(thunk)
